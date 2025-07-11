0d7bba336636e10eec0f8b7109ba72d549cd6e67
NicolaPiras97 <npiras1997@gmail.com>
2025-07-11 17:42:43 +0200

    Update Hierarchical_CUB.cpp

--- Hierarchical_CUB.cpp
+++ Hierarchical_CUB.cpp
@@ -64,10 +64,10 @@ int main2(vector<vector<int>> yy){
     _L=3;
     _R=2;
     _iter=100;
-    _iter2=100;
+    _iter2=200;
     _starting=6;
-    double eps=0.000000001;
-    double eps2=0.000000001;
+    double eps=0.000000000000001;
+    double eps2=0.0000000000000000000000000000000000000000001;
     
     
     _Q = y(0, 1);
